7b8eef0b2df9bd11dd3d9bbdbd6d5b3dcb61333e
David Cabeza <davidecl@hotmail.com>
2017-03-24 16:59:45 -0400

    Identation two spaces, get procedure

--- list.c
+++ list.c
@@ -3,95 +3,81 @@
  *
  * Autor: David Cabeza <13-10191@usb.ve>
  * Autor: Fabiola Martínez <13-10838@usb.ve>
- *  
+ *
  * Universidad Simón Bolívar
  * Caracas, Venezuela
 */
 
 #include "list.h"
 
-void init_element(Element *e, char* dname){
-	e->name = dname;
-	e->prev = NULL;
-	e->next = NULL;
+void init_node(Node *n, void *content){
+  n->next = NULL;
+  n->prev = NULL;
+  n->content = content;
 }
 
 void init_list(List *l){
-	l->first = NULL;
-	l->last = NULL;
-	l->size = 0;
+  l->first = NULL;
+  l->last = NULL;
+  l->size = 0;
 }
 
-void add(List *l, char* dname){
+void add(List *l, Node *n){
+  if(!l->first){
+    l->first = n;
+    l->last = n;
+  }
+  else{
+    l->last->next = n;
+    n->prev = l->last;
+    l->last = n;
+  }
 
-	Element *e = (Element *) malloc(sizeof(Element));
-	init_element(e, dname);
-
-	if(!l->first){
-		l->first = e;
-		l->last = e;
-	}
-	else{
-		l->last->next = e;
-		e->prev = l->last;
-		l->last = e;
-	}
-
-	l->size++;
+  l->size++;
 }
 
-void addRes(ResList *l, ResElement *r){
+Node *get(List *l){
 
-	if(!l->first){
-		l->first = r;
-		l->last = r;
-	}
-	else{
-		l->last->next = r;
-		r->prev = l->last;
-		l->last = r;
-	}
+  Node *tmp;
 
-	l->size++;
+  if(l->size == 0){
+	   printf("No hay nada\n");
+     return;
+ 	}
+
+ 	else {
+ 		tmp = l->first;
+
+ 		if(l->size == 1){
+ 			l->first = NULL;
+ 			l->last = NULL;
+ 		}
+
+    else{
+      if (!tmp->prev){
+        l->first = tmp->next;
+        l->first->prev = NULL;
+      }
+      else if (!tmp->next){
+        l->last = tmp->prev;
+        l->last->next = NULL;
+      }
+      else{
+        tmp->prev->next = tmp->next;
+ 				tmp->next->prev = tmp->prev;
+ 			}
+
+ 		}
+    l->size--;
+    return tmp;
+ 	}
 }
-
-char *get(List *l){
-
-	if(l->size == 0){
-		printf("No hay directorios \n");
-	}
-	
-	else {
-		Element *tmp = l->first;
-
-		if(l->size == 1){
-			l->first = NULL;
-			l->last = NULL;
-		}
-
-		else{
-			if (!tmp->prev){
-				l->first = tmp->next;
-				l->first->prev = NULL; 
-			}
-			else if (!tmp->next){
-				l->last = tmp->prev;
-				l->last->next = NULL;
-			}
-			else{
-				tmp->prev->next = tmp->next;
-				tmp->next->prev = tmp->prev;
-			}
-
-			l->size--;
-			return tmp->name;
-		}
-	}
-}
-
 
 bool empty(List *l){
-	
-	if(l->size) return false;
-	else return true;
+  if (l->size == 0){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
--- list.h
+++ list.h
@@ -3,103 +3,45 @@
  *
  * Autor: David Cabeza <13-10191@usb.ve>
  * Autor: Fabiola Martínez <13-10838@usb.ve>
- *  
+ *
  * Universidad Simón Bolívar
  * Caracas, Venezuela
 */
-
-
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 /*
- * Estructura para la lista de directorios a ser explorados. Almacena el nombre 
- * del directorio, un apuntador al directorio anterior y uno al siguiente.
+ * Estructura para las listas.
  *
 */
-typedef struct element{
-	char* name;
-	struct element *prev;
-	struct element *next;
-} Element;
+typedef struct node{
+	struct node *prev;
+	struct node *next;
+  void *content;
+} Node;
 
-/*
- *
- *
-*/
-typedef struct reselement{
-	long id;
-	char *path;
-	int fcount;
-	int bcount;
-	struct reselement *prev;
-	struct reselement *next;
+typedef struct directory{
+  char dir[4096];
+} Directory;
 
-} ResElement;
+typedef struct information{
+  long id;
+  char path[4096];
+  int fcount;
+  int bcount;
+} Information;
 
-/*
- * Implementación de una lista. Esta es la lista de directorios a ser explorados
- * por los hilos trabajadores. Almacena el primer elemento, el último y su tamaño.
- *
-*/
 typedef struct list{
-	Element *first;
-	Element *last;
-	int size;
-
+  Node *first;
+  Node *last;
+  int size;
 } List;
 
-/*
- *
- *
- *
- *
-*/
-typedef struct reslist{
-	ResElement *first;
-	ResElement *last;
-	int size;
+void init_node(Node *n, void *content);
 
-} ResList;
-/*
- * Inicializa el elemento que va en la lista con el nombre del directorio y
- * apuntadores al anterior y siguiente elemento.
- *
-*/
-void init_element(Element *e, char* dname);
-
-/*	
- * Inicializa la lista de directorios a ser explorados.
- *
-*/
 void init_list(List *l);
 
-/*
- * Función para añadir un elemento que contiene el directorio a ser explorado a
- * la lista l.
- *
-*/
-void add(List *l, char *dname);
-
-/*
- *
- *
- *
-*/
-void addRes(ResList *l, ResElement *r);
-
-/*
- *
- *
- *
-*/
-char *get(List *l);
-
-/*
- *
- *
- *
-*/
 bool empty(List *l);
 
+Node *get(List *l);
