0596abcabef0ecd48ab8f23874f154c8a4066b2d
David Cabeza <davidecl@hotmail.com>
2017-03-24 17:00:15 -0400

    Variable declarations in the top, new procedures

--- main.c
+++ main.c
@@ -3,70 +3,92 @@
  *
  * Autor: David Cabeza <13-10191@usb.ve>
  * Autor: Fabiola Martínez <13-10838@usb.ve>
- *  
+ *
+ * Descripción:
+ *
  * Universidad Simón Bolívar
  * Caracas, Venezuela
 */
-
-
 #include "tls.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  /*
+   * Por defecto los valores de los argumentos son inicializados mediante la
+   * función init_inputargs con 1, directorio de trabajo actual y "salida"
+   * respectivamente.
+  */
+  Inargs *in = (Inargs *) malloc(sizeof(Inargs));
+  // Lista que contendra los directorios pendientes a explorar.
+  List *dirlist = (List *) malloc(sizeof(List));
+  // Lista que contiene la informacion obtenida por los hilos.
+  List *infolist = (List *) malloc(sizeof(List));
+  // Lista que contiene los hilos que no tienen directorios asignados.
+  List *idlelist = (List *) malloc(sizeof(List));
+  // Apuntador a los hilos.
+  pthread_t *threads;
+  //
+  Threadstruct *threads_structs;
+  //
+  Threadstruct *masterthread;
+  // Contador de iteracion.
+  int i;
 
-	pthread_mutex_t dirmutex;
-	pthread_mutex_init(&dirmutex, NULL);
-	
-	/*
-	 * Por defecto los valores de los argumentos son inicializados mediante la 
-	 * función init_inputargs con 1, directorio de trabajo actual y "salida" 
-	 * respectivamente.
-	 *
+  pthread_mutex_init(&idlemutex, NULL);
+  pthread_mutex_init(&dirmutex, NULL);
+  pthread_mutex_init(&infomutex, NULL);
+
+  // Inicializamos los valores introducidos por el usuario.
+  init_inputargs(in);
+
+  // Ejecutamos la funcion que se encagara de procesar los valores dados por el
+  // usuario.
+  parseArgs(in, argc, argv);
+
+  //chdir(in->directory);
+	getcwd(in->directory, 4096);
+
+  /*
+   * Dos estructuras globales threads y threads_structs. La primera almacena los
+   * hilos, la segunda almacenara las estructuras de los hilos.
+  */
+  threads = (pthread_t *) malloc(sizeof(in->concurrency * sizeof(pthread_t)));
+  threads_structs = (Threadstruct *) malloc(sizeof(in->concurrency * sizeof(Threadstruct)));
+
+  // Inicializamos la lista donde se almacenaran los directorios a explorar.
+  init_list(dirlist);
+
+  // Inicializamos la lista donde se almacenara a inofrmacion obtenida por los
+  // hilos.
+  init_list(infolist);
+
+  // Inicializamos la lista donde se almacenaran los hilos que no tienen direc-
+  // torios asignados.
+  init_list(idlelist);
+
+  masterthread = (Threadstruct *) malloc(sizeof(Threadstruct));
+  init_threadstruct(masterthread, in->concurrency, dirlist, infolist, idlelist);
+  strcpy(masterthread->directory, in->directory);
+
+  explore(masterthread);
+
+  /*
+	 * Se crearan el numero de hilos indicado en el valor de concurrency.
+   *
+	 * Los hilos quedan esperando que el hilo maestro explore el directorio
+	 * principal.
 	*/
-	Inargs *in = (Inargs *) malloc(sizeof(Inargs));
-	init_inputargs(in);
+  //printf("Inicio de la creacion\n");
+  createThreads(in->concurrency, threads, dirlist, infolist, idlelist);
+  //printf("Fin de la creacion\n");
 
-	parseArgs(in, argc, argv);
+  //printf("Comienzo de la asignacion\n");
+  allocateDir(masterthread);
 
-	/*
-	 * Se crearan el numero de hilos indicado en el valor de concurrency. Cada
-	 * hilo tendra asignada una estructura threadstruct que contendra su id, su
-	 * estado, la lista de directorios (para añadir los directorios que encuentre),
-	 * la lista de resultados (para completar al finalizar la exploración del dir),
-	 * el mutex de directorios (evitar problemas al añadir, quitar directorios),
-	 * el mutex de resultados (mismo caso de directorios), el mutex de estado
-	 *
-	 * Las estructuras de todos los hilos se almacenan en el arreglo ts.
-	 *
-	 * Los hilos quedan esperando que el hilo maestro explore el directorio prin-
-	 * cipal. 
-	 *
-	*/	
-	List *dirlist = (List *) malloc(sizeof(List));
-	init_list(dirlist);
+  for(i = 0; i < in->concurrency; i++) pthread_join(threads[i], NULL);
 
-	pthread_t threads[in->concurrency];
-	threadstruct *tsarray[in->concurrency];
-	createThreads(in->concurrency, dirlist, dirmutex, threads, tsarray);
+  printf("salida salida %")
+  writeInformation(masterthread, in->out);
 
-	/*
-	 * El hilo maestro inicializa la lista de directorios y comienza a explorar
-	 * el directorio pasado por consola (o el directorio por defecto) agregando
-	 * los subdirectorios encontrados a la lista dirlist
-	 *
-	*/
-	explore(in->directory, dirlist);
-
-	/*
-	 *
-	 *
-	 *
-	*/
-	printf("Comienzo a asignar directorios a la gente\n");
-	allocateDir(in->concurrency, dirlist, tsarray, dirmutex);
-
-	int i;
-	for(i=0; i< in->concurrency; i++) pthread_join(threads[i], NULL);	
-
-	return 0;
+  //printf("Hola me voy soy el papa\n");
+  return 0;
 }
