ad63f22fff42b6fa379f425e4f1892878a4a68be
David Cabeza <davidecl@hotmail.com>
2017-03-25 08:10:01 -0400

    path_max, new headers and documentation

--- tls.h
+++ tls.h
@@ -4,10 +4,16 @@
  * Autor: David Cabeza <13-10191@usb.ve>
  * Autor: Fabiola Martínez <13-10838@usb.ve>
  *
+ * Descripción: Cabecera del archivo tls.c que contiene las librerías
+ * necesarias, variables globales, prototipos de estructuras y de
+ * procedimientos y métodos.
+ *
  * Universidad Simón Bolívar
  * Caracas, Venezuela
  * Marzo, 2017.
+ *
 */
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -27,35 +33,40 @@ pthread_mutex_t idlemutex, dirmutex, infomutex;
 */
 typedef struct inargs{
 	int concurrency;
-	char directory[4096];
+	char directory[PATH_MAX];
 	char out[256];
 } Inargs;
 
+/*
+ * Estructura de hilos que almacena información necesaria durante su ejecución,
+ * i.e. id de hilo, directorio asignado a explorar, número de hilos en el
+ * programa, indicador de asignación o no de directorio y apuntadores a: lista
+ * de directorios, de información encontrada, de hilos sin directorio asignado.
+*/
 typedef struct threadstruct{
   long id;
-  char directory[4096];
+  char directory[PATH_MAX];
 	int thnum;
-	int has_dirassigned;
+	int has_dirallocated;
   List *dirlist;
   List *infolist;
 	List *idlelist;
 } Threadstruct;
 
 /*
- * Ejecuta pager localizado en /usr/bin/ para visualizar el manual del
- * programa que contiene información acerca de la sintaxis, descripción
- * de párametros, etc.
+ * Ejecuta pager para visualizar el manual del programa que contiene
+ * información acerca de la sintaxis, descripción de párametros, etc.
 */
 void help(void);
 
 /*
- * Se ejecuta cuando se invoca el programa con un comando inválido.
+ * Se ejecuta cuando el programa es invocado mediante un comando inválido.
 */
 void usage(void);
 
 /*
- * Inicializa la estructura que almacenará los argumentos introducidos
- * al invocar el programa con valores por defecto.
+ * Inicializa con valores por defecto la estructura que almacenará los
+ * argumentos introducidos al invocar el programa.
 */
 void init_inputargs(Inargs *in);
 
@@ -65,16 +76,33 @@ void init_inputargs(Inargs *in);
 */
 void parseArgs(Inargs *in, int argc, char *argv[]);
 
+/*
+ * Inicializa la estructura de los hilos con parámetros y valores por defecto.
+*/
 void init_threadstruct(Threadstruct *t, int thnum, List *dirlist,
 												List* infolist, List *idlelist);
 
+/*
+ * Verifica dado el número de hilos y la lista de hilos inactivos, si hay
+ * hilos trabajando
+*/
 int onwork(int thnum, List* idlelist);
 
+/*
+ * Procedimiento ejecutado por los hilos al momento de su creación. Mantiene a
+ * los hilos en un ciclo esperando por asignacion de directorios. Al tener uno,
+ * son enviados a explorarlo.
+*/
 void *threadmgmt(void *structure);
 
-void createThreads(int numthreads, pthread_t *threads, List *dirlist,
-										List *infolist, List* idlelist);
+/*
+ * Procedimiento ejecutado por el hilo maetsro que se encarga de crear los
+ * hilos indicados en los argumentos de invocación del programa.
+*/
+void createThreads(Threadstruct *master, pthread_t *threads);
 
 void explore(Threadstruct *t);
 
 void allocateDir(Threadstruct *master);
+
+void writeInformation(Threadstruct *master, char *out);
